7a151ee297a78d07a2ccf014effa2bf591e6e2f5
YeicobGarcia <jacobgarcia.jm@gmail.com>
2023-11-17 11:16:35 -0600

    interfaz admin configurada para modelos

--- Core/Tamu/admin.py
+++ Core/Tamu/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 
-from Core.Tamu.models import TamuModel
+from Core.Tamu.models import *
 
 admin.site.register(TamuModel)
+admin.site.register(LineaModel)
+admin.site.register(LineaSeccion)
+admin.site.register(SKUModel)
+admin.site.register(AreaModel)
--- Core/Tamu/models.py
+++ Core/Tamu/models.py
@@ -5,20 +5,40 @@ from django.db import models
 class AreaModel(models.Model):
     nombre = models.CharField(max_length=50, null=True)
 
+    """def __str__(self):
+        # Devuelve una representación legible del modelo en el admin
+        texto = "{0} ({1})"
+        return texto.format(self.nombre)"""
+
 class SKUModel(models.Model):
     areaId = models.ForeignKey(AreaModel, on_delete=models.CASCADE)
     descripcion = models.CharField(max_length=100, null=True)
     humedad_objetiva = models.DecimalField(max_digits=5, decimal_places=0, null=True)
     peso_objetivo = models.DecimalField(max_digits=5, decimal_places=0,  null=True)
     temperatura_objetiva = models.DecimalField(max_digits=5, decimal_places=0,  null=True)
-
+    
+    """def __str__(self):
+        # Devuelve una representación legible del modelo en el admin
+        texto = "SKU: {0} humedad_objetiva: ({1}) peso_objetivo:({2}) temperatura_objetiva:({3})"
+        return texto.format(self.descripcion, self.humedad_objetiva, self.peso_objetivo, self.temperatura_objetiva)
+    """
 class LineaSeccion(models.Model):
     nombre = models.CharField(max_length=5, null=True)
     areaId = models.ForeignKey(AreaModel, on_delete=models.CASCADE, null=True, blank=True)
 
+    """def __str__(self):
+        # Devuelve una representación legible del modelo en el admin
+        texto = "{0}"
+        return texto.format(self.nombre)"""
+    
 class LineaModel(models.Model):
     nombre = models.CharField(max_length=100, null=True)
     areaId = models.ForeignKey(AreaModel, on_delete=models.CASCADE)
+
+    """def __str__(self):
+        # Devuelve una representación legible del modelo en el admin
+        texto = "{0}"
+        return texto.format(self.nombre)"""
 
 class TamuModel(models.Model):
     areaId = models.ForeignKey(AreaModel, on_delete=models.CASCADE, null=True)
@@ -32,3 +52,10 @@ class TamuModel(models.Model):
     temperatura_objetiva = models.DecimalField(max_digits=5, decimal_places=0, null=True)
     temperatura_obtenida = models.DecimalField(max_digits=5, decimal_places=2, null=True)
     fecha_registro = models.DateTimeField(auto_now=True)
+
+    """def __str__(self):
+        # Devuelve una representación legible del modelo en el admin
+        texto = "peso_objetivo: ({0}) peso_obtenido:({1}) humedad_objetiva:({2})  humedad_obtenida:({3}) temperatura_objetiva:({4}) temperatura_objetiva:({5}) fecha_registro:({6})"
+        return texto.format(self.peso_objetivo, self.peso_obtenido,
+                            self.humedad_objetiva, self.humedad_obtenida, self.temperatura_objetiva,
+                            self.temperatura_obtenida, self.fecha_registro,)"""
